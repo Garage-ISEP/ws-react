ae05d1b9280dc6fa4bfde996cf8a2fea96ec0528
Adrien Bethuel <adrienbethuel@gmail.com>
2023-03-09 20:40:08 +0100

    up

--- src/components/BlogList.tsx
+++ src/components/BlogList.tsx
@@ -3,7 +3,26 @@ import { BlogCard } from './BlogCard';
 
 export const BlogList = (props: any) => {
   const [data, setData] = useState(props.data);
-  console.log(data);
+  const [newTitle, setNewTitle] = useState('');
+  const [newDescription, setNewDescription] = useState('');
+  const [newImage, setNewImage] = useState('');
+
+  const addArticle = (e: any) => {
+    e.preventDefault();
+    const lastEl = data[data.length - 1];
+    const newArticle = {
+      id: lastEl.id + 1,
+      title: newTitle,
+      shortDescription: newDescription,
+      longDescription: newDescription,
+      likes: 0,
+      image: newImage,
+    };
+    const newData = [...data, { ...newArticle }];
+    setData(newData);
+    console.log('d', newData);
+  };
+
   return (
     <div className="grid place-items-center mb-12">
       {data.map((item: any) => (
@@ -16,7 +35,53 @@ export const BlogList = (props: any) => {
           image={item.image}
           key={item.id}
         />
-      ))}
+      ))}{' '}
+      <div className="w-full max-w-xs">
+        <form onSubmit={addArticle}>
+          <div className="mb-4">
+            <label className="block font-bold mb-2 text-left">
+              Ajouter un titre
+            </label>
+            <input
+              className="shadow border rounded w-full py-2 px-3"
+              id="username"
+              type="text"
+              placeholder="Titre"
+              onChange={(e) => setNewTitle(e.target.value)}
+              value={newTitle}
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block font-bold mb-2 text-left">
+              Ajouter une image
+            </label>
+            <input
+              className="shadow border rounded w-full py-2 px-3"
+              id="username"
+              type="url"
+              placeholder="Lien de l'image"
+              value={newImage}
+              onChange={(e) => setNewImage(e.target.value)}
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block font-bold mb-2 text-left">
+              Ajouter une description
+            </label>
+            <textarea
+              className="shadow border rounded w-full py-2 px-3 mb-3"
+              placeholder="Description"
+              value={newDescription}
+              onChange={(e) => setNewDescription(e.target.value)}
+            />
+          </div>
+          <div className="flex justify-between">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              Ajouter un article
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,25 +1,27 @@
 export const Header = () => {
   return (
     <header className="grid place-items-center mb-5 mt-5">
-      <div className="flex justify-around items-center	">
-        <img
-          className="object-scale-down h-16 mr-60"
-          src="https://cdn.devdojo.com/tails/images/QL2K5DTLLktLTeLfDOZ5ub5wDfXvf1NciZC15uNo.png"
-          alt="logo"
-        />
-        <div className="ml-60">
-          <a
-            href="/"
-            className="mr-5 underline text-lg font-semibold hover:text-gray-600"
-          >
-            Home
-          </a>
-          <a
-            href="/about"
-            className="underline text-lg font-semibold hover:text-gray-600"
-          >
-            About
-          </a>
+      <div className="w-1/2">
+        <div className="flex justify-between items-center	">
+          <img
+            className="object-scale-down h-20 mr-60"
+            src="https://cdn.devdojo.com/tails/images/QL2K5DTLLktLTeLfDOZ5ub5wDfXvf1NciZC15uNo.png"
+            alt="logo"
+          />
+          <div>
+            <a
+              href="/"
+              className="mr-5 underline text-lg font-semibold hover:text-gray-600"
+            >
+              Home
+            </a>
+            <a
+              href="/about"
+              className="underline text-lg font-semibold hover:text-gray-600"
+            >
+              About
+            </a>
+          </div>
         </div>
       </div>
     </header>
